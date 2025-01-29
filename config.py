3479fc76f91da7d31a5f41dc7490fe05a383704d
OrewaSanjii-Dono <sanjihunbechutiye+new56@gmail.com>
2025-01-29 22:42:47 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,61 @@
+# +++ Made By Sanjiii [telegram username: @Urr_Sanjiii] +++
+
+import asyncio
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+#Bot token @Botfather, --⚠️ REQUIRED--
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7345853150:AAGDt5mdN-Sct0O5oHQgbG6kyZ5zc_rcwOI")
+
+#Your API ID from my.telegram.org, --⚠️ REQUIRED--
+APP_ID = int(os.environ.get("APP_ID", "24567281"))
+
+#Your API Hash from my.telegram.org, --⚠️ REQUIRED--
+API_HASH = os.environ.get("API_HASH", "17a51c14e6c25caad1a8f63a97c51f96")
+
+#Your db channel Id --⚠️ REQUIRED--
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002163025446"))
+
+#OWNER ID --⚠️ REQUIRED--
+OWNER_ID = int(os.environ.get("OWNER_ID", "7827448605"))
+
+#SUPPORT_GROUP: This is used for normal users for getting help if they don't understand how to use the bot --⚠ OPTIONAL--
+SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "-1002361458256")
+
+#Port
+PORT = os.environ.get("PORT", "8080")
+
+#Database --⚠️ REQUIRED--
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://sagatobots00001:sagatobots100@cluster00001.vgdshkw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster00001")
+DB_NAME = os.environ.get("DATABASE_NAME", "aniverse_manhwa_world")
+
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+#Collection of pics for Bot // #Optional but atleast one pic link should be replaced if you don't want predefined links
+PICS = (os.environ.get("PICS", "https://envs.sh/arr.jpg https://envs.sh/ar9.jpg https://envs.sh/arH.jpg")).split() #Required
+
+#set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
